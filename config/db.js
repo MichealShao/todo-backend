b7dd90cd96b43c61b2e14d4a184a2d19d5da6ed6
Micheal S <micheals@mac.home>
2025-03-26 15:14:28 -0400

    Fix MongoDB connection environment variable

--- config/db.js
+++ config/db.js
@@ -2,12 +2,14 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    console.log('MONGO_URI =', process.env.MONGO_URI);  // Debug output
+    console.log('MongoDB URI =', process.env.MONGODB_URI);  // Debug output
 
-    await mongoose.connect(process.env.MONGO_URI);
+    await mongoose.connect(process.env.MONGODB_URI);
     console.log('MongoDB connected');
   } catch (err) {
     console.error('Connection failed:', err.message);
+    // Log more details about the error
+    console.error('Error details:', err);
     process.exit(1);
   }
 };
